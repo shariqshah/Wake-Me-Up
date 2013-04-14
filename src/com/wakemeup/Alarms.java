6b58739f1ddac549a22e5f85313838bfc0ce620f
shariq shah <bluerriq@gmail.com>
2013-04-14 23:17:28 +0500

    First commit

---
+++ src/com/wakemeup/Alarms.java
@@ -0,0 +1,17 @@
+package com.wakemeup;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Alarms extends Activity {
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarms);
+        
+        
+    }
+
+}
